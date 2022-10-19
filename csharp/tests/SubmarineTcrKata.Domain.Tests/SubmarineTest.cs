0c3321b1e5f4ae36a71fc9a2db60300d8f354b4c
guillaume servais <gservais98@gmail.com>
2022-10-19 18:35:23 +0200

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 1
    test-stats:
        run: 0
        passed: 0
        failed: 0
        skipped: 0
        error: 0
        duration: 0s

--- csharp/tests/SubmarineTcrKata.Domain.Tests/SubmarineTest.cs
+++ csharp/tests/SubmarineTcrKata.Domain.Tests/SubmarineTest.cs
@@ -40,6 +40,7 @@ public class SubmarineTest
 
     [Theory]
     [InlineData(1)]
+    [InlineData(2)]
     public void ShouldGoUp(int value)
     {
         _submarine.ExecuteCommand("up " + value);
